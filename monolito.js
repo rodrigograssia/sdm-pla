c61f7d4bf28e2bde338fc6d9d29b04af7d8aa7c2
Rodrigo Oliveira Grassia <824135663@ulife.com.br>
2025-09-12 09:22:56 -0300

    feat: monolith application

--- monolito.js
+++ monolito.js
@@ -8,22 +8,23 @@ let usuarios = [];
 let pedidos = [];
 
 // Rotas para criação de usuários
-app.post("/usuarios", (req,res) => {
+app.post("/usuarios", (req, res) => {
     const usuario = req.body;
     usuarios.push(usuario);
-    res.send({message: "Usuário criado com sucesso!"});
+    res.send({ message: "Usuário criado com sucesso!" });
 });
 
 // Rota para criar um pedido e usuários
 app.post("/pedidos", (req, res) => {
     const pedido = req.body;
     pedidos.push(pedido);
-    res.send({message: "Pedido criado com sucesso!"});
+    res.send({ message: "Pedido criado com sucesso!" });
 });
 
 // Rota para listar todos os usuários
 app.get("/dados", (req, res) => {
     res.send({ usuarios });
+    res.send({ pedidos });
 })
 
 
